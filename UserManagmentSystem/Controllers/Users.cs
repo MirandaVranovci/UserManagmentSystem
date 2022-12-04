a97554e09852d5a886d47730beb9194764e3734a
Miranda <miranda.vranovci1@gmail.com>
2022-12-04 22:04:55 +0100

    created user controller

---
+++ UserManagmentSystem/Controllers/Users.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UserManagmentSystem.Controllers
+{
+    public class Users : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
